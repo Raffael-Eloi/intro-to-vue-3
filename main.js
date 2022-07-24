a93fbbac6b52412a1831878b5d4778554c7d3f12
Raffael Silva <raffaeleloi121@gmail.com>
2022-07-24 11:39:51 -0300

    first class - creating the Vue app

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+/* It's the heart of the vue application */
+const app = Vue.createApp({
+  data () {
+    return {
+      product: 'Socks',
+      description: 'An amazing product build with Vue app',
+    }
+  }
+})
+
+/* When is created the createApp you must to pass an options or at least a empty object */
